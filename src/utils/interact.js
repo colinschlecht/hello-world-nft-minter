6ae5511f9b55e8932f00b4af2f3bb78475e903b3
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-21 21:55:56 -0700

    adds hardhat

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -1,5 +1,10 @@
 //wallet and smart contract interaction functions
 
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey); 
+
 export const connectWallet = async () => {
   if (window.ethereum) {
     //check if Metamask is installed
