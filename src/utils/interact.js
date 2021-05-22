2bf30d37a59c9dbac7f1e342a4def86baa646b7a
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-21 23:00:46 -0700

    adds mint nft function

--- src/utils/interact.js
+++ src/utils/interact.js
@@ -1,9 +1,11 @@
 //wallet and smart contract interaction functions
-require('dotenv').config();
+import { pinJSONToIPFS } from "./pinata.js";
+
+require("dotenv").config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-const web3 = createAlchemyWeb3(alchemyKey); 
-// const contract = require("../../artifacts/contracts/HelloWorldNFT.sol/HelloWorldNFT.json"); 
+const web3 = createAlchemyWeb3(alchemyKey);
+const contractABI = require("../contract-abi.json");
 const contractAddress = "0x50274041223B467Cc17030732b4A2Ab94fAFde7a";
 
 export const connectWallet = async () => {
@@ -28,6 +30,60 @@ export const connectWallet = async () => {
       connectedStatus: false,
       status:
         "🦊 You must install Metamask into your browser: https://metamask.io/download.html",
+    };
+  }
+};
+
+export const mintNFT = async (url, name, description) => {
+  if (url.trim() == "" || name.trim() == "" || description.trim() == "") {
+    return {
+      success: false,
+      status: "❗Please make sure all fields are completed before minting.",
+    };
+  }
+  //make metadata
+  const metadata = new Object();
+  metadata.name = name;
+  metadata.image = url;
+  metadata.description = description;
+
+  //make pinata call
+  const pinataResponse = await pinJSONToIPFS(metadata);
+  if (!pinataResponse.success) {
+    return {
+      success: false,
+      status: "😢 Something went wrong while uploading your tokenURI.",
+    };
+  }
+  const tokenURI = pinataResponse.pinataUrl;
+
+  window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+  //set up the Ethereum transaction
+  const transactionParameters = {
+    to: contractAddress, // Required except during contract publications.
+    from: window.ethereum.selectedAddress, // must match user's active address.
+    data: window.contract.methods
+      .mintNFT(window.ethereum.selectedAddress, tokenURI)
+      .encodeABI(), //make call to NFT smart contract
+  };
+
+  //sign the transaction via Metamask
+  try {
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [transactionParameters],
+    });
+    return {
+      success: true,
+      status:
+        "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+        txHash,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: "😥 Something went wrong: " + error.message,
     };
   }
 };
